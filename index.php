4e69316093712511d5940d954aefdbecd7331012
piettra <piettra_cidral@estudante.sesisenai.org.br>
2026-09-17 12:47:48 -0300

    add- style  em andamento

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if ($resultado->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página login</title>
-   <link rel="stylesheet" href="assets/style/style.css">
+   <link rel="stylesheet" href="../assets/style/style.css">
 
 </head>
 
