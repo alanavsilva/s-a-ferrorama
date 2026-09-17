628853853f007070780417acd7f6775f96591d77
piettra <piettra_cidral@estudante.sesisenai.org.br>
2026-09-17 09:14:38 -0300

    add- arrumando erros

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include "conexao.php";
+include  "../infra/conexao.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -37,7 +37,7 @@ if ($resultado->num_rows > 0) {
     <title>Página login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/style/style.css">
+    <link rel="stylesheet" href="../../assets/style/style.css">
 
 </head>
 
