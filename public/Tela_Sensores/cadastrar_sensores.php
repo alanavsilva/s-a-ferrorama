f4ab707f861356671cb0b643274c2ba91727f320
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-09-24 11:31:25 -0300

    add- coloquei e comecei o php

--- public/Tela_Sensores/cadastrar_sensores.php
+++ public/Tela_Sensores/cadastrar_sensores.php
@@ -124,7 +124,7 @@
               <option>Rota Oeste</option>
             </select>
           </div>
-          <button class="botao" type="submit">Cadastrar Sensor</button>
+          <button class="botao_largo" type="submit">Cadastrar Sensor</button>
         </form>
       </details>
       
